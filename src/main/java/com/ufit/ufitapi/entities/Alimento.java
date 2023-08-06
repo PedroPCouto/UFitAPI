86d37d8046cb7e3f61ea74fd4a855d2b7f451202
PedroPCouto <pedrohpaiva1258@gmail.com>
2023-08-06 16:58:47 -0300

    Integração com postgres local

--- src/main/java/com/ufit/ufitapi/entities/Alimento.java
+++ src/main/java/com/ufit/ufitapi/entities/Alimento.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-@Table(name = "Alimento")
+@Table(name = "tb_alimentos")
 public class Alimento implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
